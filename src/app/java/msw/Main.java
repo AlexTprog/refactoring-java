c98dc6ad111bd6e0c158fadad3db67db9dff763c
Alexander Torre <alexander.torre.arteaga@gmail.com>
2023-07-12 15:53:48 -0500

    parametrize movies

--- src/app/java/msw/Main.java
+++ src/app/java/msw/Main.java
@@ -1,14 +1,22 @@
 package app.java.msw;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+    HashMap<String, Movie> movies = new HashMap();
+
+    movies.put("F001", new Movie("You've Got Mail", "regular"));
+    movies.put("F002", new Movie("Matrix", "regular"));
+    movies.put("F003", new Movie("Cars", "childrens"));
+    movies.put("F004", new Movie("Fast & Furious X", "new"));
 
     String result = new RentalInfo()
-        .statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        .statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))),
+            movies);
 
     if (!result.equals(expected)) {
       throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator()
